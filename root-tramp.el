dc55d5b1a907db1ae07a3d61e3f953192a8ea993
ncaq <ncaq@ncaq.net>
2013-12-11 06:26:43 +0900

    sudoedit-and-closeを改良して,diredの時にも使えるようにした

--- root-tramp.el
+++ root-tramp.el
@@ -1,22 +1,26 @@
-;;; -*- coding: utf-8; lexical-binding: t -*-
 (require 'tramp)
-;;本当は,ファイル開く→前のファイル消す という順番にしたい けれど,名前を変えた後のBufferを,Uniq識別方法が違っても消す方法がわからない.
+(require 'dired)
+
+(defun sudoedit (source-path)
+  (let ((tramp-path (concat "/sudo::" source-path)))
+    (find-file tramp-path)))
+
 (defun sudoedit-and-close ()
   (interactive)
-  (setq nowrite-buffer-name (buffer-name))
-  (setq nowrite-file-name (buffer-file-name))
-  (setq tramp-buffer-name (concat "/sudo::" nowrite-file-name))
-  (kill-buffer nowrite-buffer-name)
-  (find-file tramp-buffer-name)
-  (message "sudoedit done"))
+  (let ((kill-buffer-name (buffer-name))
+	(current-mode major-mode)
+	(file-name (buffer-file-name))
+	(dir-name (ignore-errors (dired-current-directory))))
+    (kill-buffer kill-buffer-name)
+    (if (eq current-mode 'dired-mode)
+	(sudoedit dir-name)
+      (sudoedit file-name))))
 
-(defun auto-tramp ()
-  (if
-      (and
-       (not (string-match "^/sudo:" (buffer-file-name)))
-       (not (file-writable-p (buffer-file-name))))
-      (sudoedit-and-close)))
+(defun auto-tramp-file ()
+  (unless (or (string-match "^/sudo::" (buffer-file-name))
+	      (file-writable-p (buffer-file-name)))
+    (sudoedit-and-close)))
 
-(add-hook 'find-file-hook 'auto-tramp)
+(add-hook 'find-file-hook 'auto-tramp-file)
 
 (provide 'root-tramp)
