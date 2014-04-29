088f17f1bc054d8ebe674130029b58ddb864e421
ncaq <ncaq@ncaq.net>
2014-04-30 02:13:24 +0900

    Refactoring

--- root-tramp.el
+++ root-tramp.el
@@ -1,26 +1,26 @@
 (require 'tramp)
-(require 'dired)
 
-(defun sudoedit (source-path)
-  (let ((tramp-path (concat "/sudo::" source-path)))
-    (find-file tramp-path)))
+(defun tramp-path (s)
+  (concat "/sudo::" s))
+
+(defun sudoedit (s)
+  (interactive "i")
+  (if s
+      (find-file (tramp-path s))
+    (find-file (tramp-path (buffer-file-name)))))
 
 (defun sudoedit-and-close ()
   (interactive)
-  (let ((kill-buffer-name (buffer-name))
-	(current-mode major-mode)
-	(file-name (buffer-file-name))
-	(dir-name (ignore-errors (dired-current-directory))))
-    (kill-buffer kill-buffer-name)
-    (if (eq current-mode 'dired-mode)
-	(sudoedit dir-name)
-      (sudoedit file-name))))
+  (let ((old-buffer-name (buffer-file-name)))
+    (kill-buffer (current-buffer))
+    (sudoedit old-buffer-name)))
 
-(defun auto-tramp-file ()
-  (unless (or (string-match "^/sudo:" (buffer-file-name))
-	      (file-writable-p (buffer-file-name)))
+(defun auto-sudoedit-and-close ()
+  (if (or (file-writable-p (buffer-file-name))
+          (tramp-tramp-file-p (buffer-file-name)))
+      ()
     (sudoedit-and-close)))
 
-(add-hook 'find-file-hook 'auto-tramp-file)
+(add-hook 'find-file-hook 'auto-sudoedit-and-close)
 
 (provide 'root-tramp)
