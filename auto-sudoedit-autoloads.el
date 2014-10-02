ac85093796542bbca80c3ca209c3220291d09aa2
ncaq <ncaq@ncaq.net>
2014-10-02 17:40:11 +0900

    supported: autoloads

---
+++ auto-sudoedit-autoloads.el
@@ -0,0 +1,24 @@
+;;; auto-sudoedit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (auto-sudoedit) "auto-sudoedit" "auto-sudoedit.el"
+;;;;;;  (21549 3352 713640 420000))
+;;; Generated autoloads from auto-sudoedit.el
+
+(autoload 'auto-sudoedit "auto-sudoedit" "\
+
+
+\(fn)" nil nil)
+
+;;;***
+
+(provide 'auto-sudoedit-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; auto-sudoedit-autoloads.el ends here
