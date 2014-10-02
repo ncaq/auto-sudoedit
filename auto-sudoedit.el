97416e45336a9643bef6e74525ad8bf6ed1c7e9b
ncaq <ncaq@ncaq.net>
2014-10-02 17:40:11 +0900

    supported: autoloads

--- auto-sudoedit.el
+++ auto-sudoedit.el
@@ -1,8 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
-(require 'tramp)
-
 (require 'f)
+(require 'tramp)
 
 (defun tramp-path (s)
   (concat "/sudo::" s))
@@ -23,6 +22,7 @@
     (kill-this-buffer)
     (sudoedit-1 old-buffer-name)))
 
+;;;###autoload
 (defun auto-sudoedit ()
   (if (or (f-writable? (current-path))
           (tramp-tramp-file-p (current-path)))
